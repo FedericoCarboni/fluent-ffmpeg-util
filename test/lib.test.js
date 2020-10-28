50d5cef1dc8bad4f54443b4524e3961f944f71a9
FedericoCarboni <fr3ddydev@gmail.com>
2020-10-28 21:31:13 +0100

    test: improve tests for handleInputStream and handleOutputStream

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,32 +1,79 @@
 'use strict';
-/* global it */
+/* global describe, it */
 
-var lib = require('../lib/lib');
+var lib = require('../lib/lib.cjs');
+var ffmpeg = require('fluent-ffmpeg');
 
 var stream = require('stream');
 var net = require('net');
+var fs = require('fs');
 var chai = require('chai');
 
-it('handleOutputStream()', function (done) {
-    var stream_1 = new stream.PassThrough();
-    var handle = lib.handleOutputStream(stream_1);
-    var connection = net.connect(handle.path.slice(5));
-    connection.write('ping', 'utf8');
-    stream_1.on('data', function (chunk) {
-        chai.expect(chunk.toString()).to.equal('ping');
-        connection.end();
-        done();
+describe('handleOutputStream()', function () {
+    it('should create a socket server', function (done) {
+        var stream_1 = new stream.PassThrough();
+        var handle = lib.handleOutputStream(stream_1);
+        var connection = net.connect(handle.path.slice(5));
+        connection.write('ping', 'utf8');
+        stream_1.on('data', function (chunk) {
+            chai.expect(chunk.toString()).to.equal('ping');
+            connection.end();
+            done();
+        });
+    });
+    it('should work through fluent-ffmpeg', function (done) {
+        var output = lib.handleOutputStream(fs.createWriteStream('test/samples/output.mkv'));
+        ffmpeg()
+            .addOption('-y')
+            .input('test/samples/video.mkv')
+            .output(output.path)
+            .videoCodec('copy')
+            .outputFormat('matroska')
+            .on('error', function (err) {
+                output.close();
+                fs.unlinkSync('test/samples/output.mkv');
+                done(err);
+            })
+            .on('end', function() {
+                output.close();
+                fs.unlinkSync('test/samples/output.mkv');
+                done();
+            })
+            .run();
     });
 });
-it('handleInputStream()', function (done) {
-    var stream_1 = new stream.PassThrough();
-    var handle = lib.handleInputStream(stream_1);
-    var connection = net.connect(handle.path.slice(5));
-    stream_1.write('ping', 'utf8');
-    connection.on('data', function (chunk) {
-        chai.expect(chunk.toString()).to.equal('ping');
-        stream_1.end();
-        done();
+describe('handleInputStream()', function () {
+    it('should create a socket server', function (done) {
+        var stream_1 = new stream.PassThrough();
+        var handle = lib.handleInputStream(stream_1);
+        var connection = net.connect(handle.path.slice(5));
+        stream_1.write('ping', 'utf8');
+        connection.on('data', function (chunk) {
+            chai.expect(chunk.toString()).to.equal('ping');
+            stream_1.end();
+            done();
+        });
+    });
+    it('should work through fluent-ffmpeg', function (done) {
+        var input = lib.handleInputStream(fs.createReadStream('test/samples/video.mkv'));
+        var output = lib.handleOutputStream(new stream.PassThrough());
+        ffmpeg()
+            .input(input.path)
+            .addInputOption('-y')
+            .output('pipe:1')
+            .videoCodec('copy')
+            .outputFormat('matroska')
+            .on('error', function (err) {
+                input.close();
+                output.close();
+                done(err);
+            })
+            .on('end', function() {
+                input.close();
+                output.close();
+                done();
+            })
+            .run();
     });
 });
 it('tee()', function () {
