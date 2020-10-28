b650d12ff863c3c4f8f58c09d28da0a5c2256f24
FedericoCarboni <fr3ddydev@gmail.com>
2020-10-28 18:51:04 +0100

    fix: check if ffmpeg is running

--- src/abort.js
+++ src/abort.js
@@ -1,3 +1,7 @@
 export function abort(command) {
+    if (!command.ffmpegProc) {
+        command.logger.warn('[util] Cannot abort ffmpeg, no process found');
+        return;
+    }
     command.ffmpegProc.stdin.write(new Uint8Array([113, 13, 10]));
 }
