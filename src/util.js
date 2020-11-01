b49deb7e25aa79a29d4f9773ab72873b2e88fb7c
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-01 09:55:15 +0100

    style: fix code style

--- src/util.js
+++ src/util.js
@@ -2,8 +2,10 @@ function _escape(c) {
     return '\\' + c;
 }
 
+var ESCAPE_REGEX = /[\\' |[\]]/g;
+
 export function escapeTeeComponent(s) {
-    return ('' + s).replace(/[\\' |[\]]/g, _escape);
+    return ('' + s).replace(ESCAPE_REGEX, _escape);
 }
 
 export var isWin32 = process.platform === 'win32';
